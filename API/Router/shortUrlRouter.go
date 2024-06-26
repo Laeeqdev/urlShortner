141ea3417e2e3dfdc541065faa5abf0c4071f2ae
Mo Laeeq <mo@devtron.ai>
2024-06-26 16:04:37 +0530

    implented all the routers

---
+++ API/Router/shortUrlRouter.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	resthandler "github.com/Laeeqdev/urlShortner/API/RestHandler"
+	"github.com/gorilla/mux"
+)
+
+type Router interface {
+	MyRouter() *mux.Router
+}
+type RouterImpl struct {
+	shortUrlHandler resthandler.ShortUrlHandler
+}
+
+func NewRouterImpl(shortUrlHandler resthandler.ShortUrlHandler) *RouterImpl {
+	return &RouterImpl{shortUrlHandler: shortUrlHandler}
+}
+
+func (impl *RouterImpl) MyRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/shorten", impl.shortUrlHandler.ShortenUrl).Methods("POST")
+	r.HandleFunc("/lengthen", impl.shortUrlHandler.LengthenUrl).Methods("POST")
+	r.HandleFunc("/{shortUrl}", impl.shortUrlHandler.Redirect).Methods("GET")
+	r.HandleFunc("/", impl.shortUrlHandler.Redirect).Methods("GET")
+	return r
+}
